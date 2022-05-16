a1f23fda1599403647f2536054922cbea1c5be00
Jose Bailo <jmbailo@gmail.com>
2022-05-16 19:53:29 +0100

    Mix Plateau and PlateauCoordinates entities
    The PlateauCoordinates didn't make sense so I mixed it with the Plateau
    entity. It only added more complexity.

--- app/Entities/Plateau.php
+++ app/Entities/Plateau.php
@@ -2,22 +2,39 @@
 
 namespace App\Entities;
 
+use App\Exceptions\InvalidXUpperRightCoordinate;
+use App\Exceptions\InvalidYUpperRightCoordinate;
+
 class Plateau
 {
+    private int $xBottomLeftCoordinate = 0;
+    private int $yBottomLeftCoordinate = 0;
     private array $occupiedPositions;
 
     public function __construct(
-        private PlateauCoordinates $coordinates
+        private int $xUpperRightCoordinate,
+        private int $yUpperRightCoordinate,
     ) {
+        if ($xUpperRightCoordinate <= $this->xBottomLeftCoordinate) {
+            throw new InvalidXUpperRightCoordinate;
+        }
+
+        if ($yUpperRightCoordinate <= $this->yBottomLeftCoordinate) {
+            throw new InvalidYUpperRightCoordinate;
+        }
+
         $this->occupiedPositions = [];
     }
 
     public function positionIsValid(Position $position): bool
     {
-        return $position->getX() >= $this->coordinates->getXBottomLeftCoordinate() &&
-            $position->getX() <= $this->coordinates->getXUpperRightCoordinate() &&
-            $position->getY() >= $this->coordinates->getYBottomLeftCoordinate() &&
-            $position->getY() <= $this->coordinates->getYUpperRightCoordinate();
+        $positionX = $position->getX();
+        $positionY = $position->getY();
+
+        return $positionX >= $this->xBottomLeftCoordinate &&
+            $positionX <= $this->xUpperRightCoordinate &&
+            $positionY >= $this->yBottomLeftCoordinate &&
+            $positionY <= $this->yUpperRightCoordinate;
     }
 
     public function setOccupiedPositions(array $positions): void
--- tests/Unit/PlateauTest.php
+++ tests/Unit/PlateauTest.php
@@ -3,8 +3,9 @@
 namespace Tests\Unit;
 
 use App\Entities\Plateau;
-use App\Entities\PlateauCoordinates;
 use App\Entities\Position;
+use App\Exceptions\InvalidXUpperRightCoordinate;
+use App\Exceptions\InvalidYUpperRightCoordinate;
 use PHPUnit\Framework\TestCase;
 
 class PlateauTest extends TestCase
@@ -12,7 +13,7 @@ class PlateauTest extends TestCase
     /** @test */
     public function check_if_some_position_are_valid()
     {
-        $plateau = new Plateau(new PlateauCoordinates(5, 5));
+        $plateau = new Plateau(5, 5);
         $this->assertTrue($plateau->positionIsValid(new Position(1, 2)));
         $this->assertTrue($plateau->positionIsValid(new Position(0, 0)));
         $this->assertTrue($plateau->positionIsValid(new Position(5, 5)));
@@ -24,7 +25,7 @@ class PlateauTest extends TestCase
     /** @test */
     public function check_if_some_position_are_occupied()
     {
-        $plateau = new Plateau(new PlateauCoordinates(5, 5));
+        $plateau = new Plateau(5, 5);
         $plateau->setOccupiedPositions([
             new Position(1, 2),
             new Position(5, 4),
@@ -33,5 +34,19 @@ class PlateauTest extends TestCase
         $this->assertTrue($plateau->positionIsOccupied(new Position(5, 4)));
         $this->assertFalse($plateau->positionIsOccupied(new Position(1, 3)));
         $this->assertFalse($plateau->positionIsOccupied(new Position(0, 0)));
+    }
+
+    /** @test */
+    public function the_x_upper_right_coordinate_must_be_greater_than_zero()
+    {
+        $this->expectException(InvalidXUpperRightCoordinate::class);
+        $plateau = new Plateau(0, 1);
+    }
+
+    /** @test */
+    public function the_y_upper_right_coordinate_must_be_greater_than_zero()
+    {
+        $this->expectException(InvalidYUpperRightCoordinate::class);
+        $plateau = new Plateau(1, 0);
     }
 }
