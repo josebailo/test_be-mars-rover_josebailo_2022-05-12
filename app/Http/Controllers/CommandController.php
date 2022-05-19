9e1409dc0530b5c561753cab726e7c540118c39b
Jose Bailo <jmbailo@gmail.com>
2022-05-19 01:27:36 +0100

    Really simulate the instructions
    The controller CommandController now uses the ParseInstructionsService
    and SimulationService to simulate the instructions given in the request.

--- app/Http/Controllers/CommandController.php
+++ app/Http/Controllers/CommandController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Rules\Instructions;
+use App\Services\ParseInstructionsService;
+use App\Services\SimulationService;
 use Illuminate\Http\Request;
 
 class CommandController extends Controller
 {
-    public function __invoke(Request $request)
-    {
+    public function __invoke(
+        Request $request,
+        ParseInstructionsService $parseInstructionsService,
+        SimulationService $simulationService,
+    ) {
         $request->validate([
             'instructions' => ['required', new Instructions],
         ]);
+        $instructions = $parseInstructionsService->parse($request->instructions);
+        $output = $simulationService->simulate($instructions);
 
-        return ['output' => "1 3 N\n5 1 E"];
+        return ['output' => $output];
     }
 }
